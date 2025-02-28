817772938859acd4bb099e3fb670a493d4cbdee0
habeeb <habeebomotolani2007@gmail.com>
2025-02-28 03:26:43 +0100

    updated backend prod setup

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ def create_app():
     app = Quart(__name__)
     app = cors(
         app, 
-        allow_origin=["http://localhost:5173", "http://127.0.0.1:5173"],
+        allow_origin=["http://localhost:5173", "http://127.0.0.1:5173", "https://www.habeebsalami.com"],
         allow_credentials=True
     )
     app.config.from_object(ConfigSetup)
@@ -40,15 +40,3 @@ async def setup():
     except Exception as e:
         print(f"Redis connection error: {e}")
         app.config["REDIS_CONNECTED"] = False
-
-if __name__ == "__main__":
-    import asyncio
-    from hypercorn.config import Config
-    from hypercorn.asyncio import serve
-
-    config = Config()
-    if os.getenv("SECURE", "false").lower() == "true":
-        config.bind = ["0.0.0.0:8000"]
-    else:
-        config.bind = ["localhost:5000"]
-    asyncio.run(serve(app, config))
